2c64498d24433407157fb36ae27b2e66e2498ed9
zeorca <wierto08@gmail.com>
2021-04-15 23:16:07 +0800

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 module.exports = {
-  theme: {
-    // Some useful comment
-  },
-  variants: {
-    // Some useful comment
-  },
-  plugins: [
-    // Some useful comment
-  ]
-}
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        fontFamily: {
+            body: [
+                "Noto Sans",
+                "Segoe UI",
+                "Roboto",
+                "Helvetica Neue",
+                "Arial",
+            ],
+        },
+        extend: {
+            backgroundImage: (theme) => ({
+                "about": "url('../img/bg-about.png')",
+            }),
+            colors: {
+                gray: {
+                    150: "#EFF0F2",
+                },
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
